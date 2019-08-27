364b2f58884ebebb66c6252283d64101173d763f
Super TZ <pyroproctos@hotmail.fr>
2019-08-27 16:25:57 +0200

    Fin du tuto 1

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -17,9 +20,22 @@ class ArticleController extends AbstractController
     }
 
     /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     * @param $slug
+     * @param LoggerInterface $logger
+     * @return JsonResponse
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        $logger->info('Article bla-bla-coeur, ça marche pas.');
+        return $this->json(['hearts' => rand(5,100)]);
+        // TODO : un truc qui marche.
+    }
+
+    /**
      * @Route("/art/{slug}", name="article_show")
      */
-    public function show($slug)
+    public function show($slug, Environment $twigEnvironment)
     {
         $comments = [
             [
@@ -31,9 +47,12 @@ class ArticleController extends AbstractController
             ],
         ];
 
-        return $this->render('art/show.html.twig', [
+        $html = $twigEnvironment->render('art/show.html.twig', [
             'comments' => $comments,
             'title' => 'Le Nanana de l\'Elfe !',
+            'slug' => $slug,
         ]);
+
+        return new Response($html);
     }
 }
